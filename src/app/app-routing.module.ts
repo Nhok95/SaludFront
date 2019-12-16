f7b89996e3d49b4d13a46c21ac926714aab4f68c
Nhok95 <enrique_1095@hotmail.es>
2019-12-16 11:36:12 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BusinessComponent } from './components/business/business.component';
+import { ContratacionComponent } from './components/contratacion/contratacion.component';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [      
+  {path:'', redirectTo: 'home', pathMatch: 'full'},  
+  {path: 'home', component: MainComponent},   //main page
+  {path: 'profile', component: ProfileComponent},  //userprofile data
+  {path: 'business', component: BusinessComponent}, // business offers
+  {path: 'contratacion', component:ContratacionComponent}, //get a contract with Generali
+  {path: 'chatbot', component:ChatbotComponent}, //talk with euGene Rali
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// Modules
+import { BrowserModule }        from '@angular/platform-browser'; //
+import { FormsModule }          from '@angular/forms';            // 2-way data binding
+import { AppRoutingModule }     from './app-routing.module';      // router-outlet
+import { PrimengModule }        from './primeng.module';          // primeng
+
+//Components
+import { AppComponent }          from './app.component';
+import { MainComponent }         from './components/main/main.component';
+import { ProfileComponent }      from './components/profile/profile.component';
+import { BusinessComponent }     from './components/business/business.component';
+import { ContratacionComponent } from './components/contratacion/contratacion.component';
+
+//Services
+
+//Pipes
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProfileComponent,
+    BusinessComponent,
+    ContratacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule, 
+    AppRoutingModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
